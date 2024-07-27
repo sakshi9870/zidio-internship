0a19fdc9ad0ead64e4853aaf8f6892aeefd4ffc4
SAKSHI SANDEEP VICHARE <132152722+sakshi9870@users.noreply.github.com>
2024-07-28 00:24:24 +0600

    Create D:\resume builder\client\src\components\Skills.js

---
+++ D:\resume builder\client\src\components\Skills.js
@@ -0,0 +1,19 @@
+// src/components/Skills.js
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <div>
+      <h3>Skills</h3>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Skills;
